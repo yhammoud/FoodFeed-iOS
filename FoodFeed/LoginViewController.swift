6e4d15025986d4063f167621614a0e56ab2b4541
Youssef Hammoud <yhammoud93@gmail.com>
2016-05-29 17:39:45 -0400

    Added Firebase Service to track loggedin users. Login, Sign up and Log out all completely functioning now

--- FoodFeed/LoginViewController.swift
+++ FoodFeed/LoginViewController.swift
@@ -11,31 +11,37 @@ import UIKit
 import Firebase
 
 class LoginViewController: UIViewController {
-    var ref = Firebase(url: "https://foodfeedapp.firebaseIO.com")
     
     @IBOutlet weak var email: UITextField!
     @IBOutlet weak var password: UITextField!
     @IBAction func loginAction(sender: UIButton) {
-        self.ref.authUser(email.text, password: password.text, withCompletionBlock: {
-            (error, auth) -> Void in
-            if (error == nil) {
-                let viewController:UIViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("Home")
-                self.presentViewController(viewController, animated: true, completion: nil)
-
-            } else {
-                let alert = UIAlertController(title: "Invalid Email or password", message: "Please enter a valid email address and password", preferredStyle: .Alert)
-                let defaultAction = UIAlertAction(title: "OK", style: .Default, handler: nil)
-                alert.addAction(defaultAction)
-                self.presentViewController(alert, animated: true, completion: nil)
-                self.email.text = ""
-                self.password.text = ""
-            }
-        })
+        if (self.email.text != nil && self.password.text != nil) {
+            FIREBASE_REF.authUser(email.text, password: password.text, withCompletionBlock: {
+                (error, auth) -> Void in
+                if (error == nil) {
+                    NSUserDefaults.standardUserDefaults().setValue(auth.uid, forKey: "uid")
+                    let viewController:UIViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("Home")
+                    self.presentViewController(viewController, animated: true, completion: nil)
+                } else {
+                    let alert = UIAlertController(title: "Invalid Email or password", message: "Please enter a valid email address and password", preferredStyle: .Alert)
+                    let defaultAction = UIAlertAction(title: "OK", style: .Default, handler: nil)
+                    alert.addAction(defaultAction)
+                    self.presentViewController(alert, animated: true, completion: nil)
+                    self.email.text = ""
+                    self.password.text = ""
+                }
+            })
+        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        if ((NSUserDefaults.standardUserDefaults().valueForKey("uid") != nil) && (CURRENT_USER.authData != nil)) {
+            let storyboard = UIStoryboard(name: "Main", bundle: NSBundle.mainBundle())
+            let destination = storyboard.instantiateViewControllerWithIdentifier("Home") as! HomeViewController
+            navigationController?.pushViewController(destination, animated: true)
+        }
     }
     
     override func didReceiveMemoryWarning() {
